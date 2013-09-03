aaf81cececf2553d95e39817e694609adf0d86f6
qingsong.jiang <mgeer@126.com>
2013-09-03 21:40:59 +0800

    read sqlite in android spike done

--- housewhere.android/src/com/sjl/housewhere/MyActivity.java
+++ housewhere.android/src/com/sjl/housewhere/MyActivity.java
@@ -1,7 +1,12 @@
 package com.sjl.housewhere;
 
 import android.app.Activity;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MyActivity extends Activity {
     /**
@@ -10,6 +15,18 @@ public class MyActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        AssetsDatabaseManager.initManager(getApplication());
+        AssetsDatabaseManager mg = AssetsDatabaseManager.getManager();
+        SQLiteDatabase database = mg.getDatabase("estates.db");
+        Cursor cursor = database.rawQuery("SELECT * FROM estates LIMIT 5", new String[0]);
+        Log.i("db", "trying to access rows in db!");
+        while(cursor.moveToNext()){
+            String name = cursor.getString(cursor.getColumnIndex("name"));
+            Log.i("db", name);
+        }
+        database.close();
         setContentView(R.layout.main);
+
     }
 }
